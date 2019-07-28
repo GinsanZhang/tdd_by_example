9e791becefd1b1fb38d17bc43f0949dbac4e1648
ginsan <zerozoogle@gmail.com>
2019-07-28 16:42:46 +0800

    [zz] chapter 6-2 remove duplicate code

--- src/main/java/com/ginsan/tdd/Franc.java
+++ src/main/java/com/ginsan/tdd/Franc.java
@@ -1,27 +1,13 @@
 package com.ginsan.tdd;
 
-import java.util.Objects;
-
-public class Franc {
-
-    private int amount;
+public class Franc extends Money {
 
     public Franc(int amount) {
-        this.amount = amount;
+        super(amount);
     }
 
     public Franc times(int multiplier) {
         return new Franc(amount * multiplier);
     }
 
-    @Override
-    public boolean equals(Object obj) {
-        Franc dollar = (Franc) obj;
-        return amount == dollar.amount;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(amount);
-    }
 }
--- src/test/java/com/ginsan/tdd/MultiCurrencyTest.java
+++ src/test/java/com/ginsan/tdd/MultiCurrencyTest.java
@@ -39,4 +39,16 @@ public class MultiCurrencyTest {
         assertThat(five.times(2)).isEqualTo(new Franc(10));
         assertThat(five.times(3)).isEqualTo(new Franc(15));
     }
+
+    @Test
+    public void should_be_equal() {
+        // given
+        // when
+        // then
+        assertThat(new Dollar(5)).isEqualTo(new Dollar(5));
+        assertThat(new Dollar(5)).isNotEqualTo(new Dollar(6));
+        assertThat(new Franc(5)).isEqualTo(new Franc(5));
+        assertThat(new Franc(5)).isNotEqualTo(new Franc(6));
+
+    }
 }
