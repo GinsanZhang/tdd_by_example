1a8986310aff569bc5a75e608444fe2cf8f7d640
ginsan <zerozoogle@gmail.com>
2019-07-28 16:07:05 +0800

    [zz] chapter 3

--- src/main/java/com/ginsan/tdd/Dollar.java
+++ src/main/java/com/ginsan/tdd/Dollar.java
@@ -1,13 +1,26 @@
 package com.ginsan.tdd;
 
+import java.util.Objects;
+
 public class Dollar {
     public int amount;
 
     public Dollar(int amount) {
-        this.amount=amount;
+        this.amount = amount;
     }
 
-    public void times(int multiplier) {
-        this.amount *= multiplier;
+    public Dollar times(int multiplier) {
+        return new Dollar(amount * multiplier);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Dollar dollar = (Dollar) obj;
+        return amount == dollar.amount;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(amount);
     }
 }
--- src/test/java/com/ginsan/tdd/MultiCurrencyTest.java
+++ src/test/java/com/ginsan/tdd/MultiCurrencyTest.java
@@ -1,20 +1,28 @@
 package com.ginsan.tdd;
 
 import org.junit.Test;
-import  static  org.assertj.core.api.Assertions.assertThat;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class MultiCurrencyTest {
 
     // 当瑞士法郎和美元当汇率为2：1时，5美元+10法郎=10美元
-    // todo 5美元*2=10美元
+    //  5美元*2=10美元 --done
+    // todo amount 定义为私有
+    // Dollar 类有副作用？
+    // todo 钱必须是 int？
+    // 实现equals()
+    // 实现 hashCode()
+    // todo 和空对象判等
+    // todo 和非同类对象判等
 
     @Test
     public void should_support_multiplication() {
         // given
         Dollar five = new Dollar(5);
         // when
-        five.times(2);
         // then
-        assertThat(five.amount).isEqualTo(10);
+        assertThat(five.times(2)).isEqualTo(new Dollar(10));
+        assertThat(five.times(3)).isEqualTo(new Dollar(15));
     }
 }
